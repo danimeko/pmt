e3b7431dfa0603f6690f4005b2f7b794f94275f4
Daniel <daniel.mengistu@hotmail.com>
2018-11-25 20:02:34 +0200

    custom validation implemented

---
+++ src/main/java/com/danimeko/pmt/exceptions/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.danimeko.pmt.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+@RestController
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request){
+        ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
+         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/danimeko/pmt/services/ProjectService.java
+++ src/main/java/com/danimeko/pmt/services/ProjectService.java
@@ -1,9 +1,11 @@
 package com.danimeko.pmt.services;
 
 import com.danimeko.pmt.domain.Project;
+import com.danimeko.pmt.exceptions.ProjectIdException;
 import com.danimeko.pmt.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Service
 public class ProjectService {
@@ -12,10 +14,13 @@ public class ProjectService {
     private ProjectRepository  projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 
-        // logic part
+            return projectRepository.save(project);
+        }catch(Exception  e){
+            throw new ProjectIdException("Project Id ' " + project.getProjectIdentifier().toUpperCase() +"' already exist");
+        }
 
-
-        return projectRepository.save(project);
     }
 }
